09b24f337c39570698210dc081b6c0f45a5cf34c
Andre Borges Medeiros <andborges@gmail.com>
2023-09-21 11:53:46 -0300

    Added recaptcha; Design changes

--- pages/api/sendMatic.ts
+++ pages/api/sendMatic.ts
@@ -1,18 +1,29 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
 
-  console.log('fala tu')
-
-  console.log(req.connection.remoteAddress) 
-  console.log(req.socket.remoteAddress)
-
-  
-
-  res.status(200).json({ });
+  try {
+    const response = await axios.post(
+      `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRETKEY}&response=${req.body.captchaToken}`
+    );
+    
+    if (response.data.success) {
+      //reCaptcha verification successfull
+      res.status(200).json({
+        transactionHash: '123',
+      });
+    } else {
+      // reCAPTCHA verification failed
+      res.status(400).send('reCAPTCHA verification failed.');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal server error');
+  }
 };
 
 export default handler;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,52 @@
 import dynamic from "next/dynamic";
-import { useState } from "react";
+import { Fragment, RefObject, useRef, useState } from "react";
 import axios from 'axios';
+import ReCAPTCHA from 'react-google-recaptcha';
+import { Dialog, Transition } from '@headlessui/react'
 
 const HomeBanner = dynamic(() => import("@/components/HomeBanner"), {
   ssr: false,
 });
 
 export default function Home() {
-  const [userAddress, setUserAddress] = useState<string>();
+  const [userAddress, setUserAddress] = useState<string>("");
+  const [captchaToken, setCaptchaToken] = useState<string>();
+  const [transactionHash, setTransactionHash] = useState<string>("");
+  const [isOpen, setIsOpen] = useState(false);
+  const recaptcha: RefObject<ReCAPTCHA> = useRef(null);
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
 
   const handleSendMatic = async (e) => {
     e.preventDefault();
-    axios.post('/api/sendMatic', {
-      userAddress
-    })
-    console.log({userAddress})
+    const response = await axios.post('/api/sendMatic', {
+      userAddress,
+      captchaToken
+    });
+
+    setTransactionHash(response.data.transactionHash);
+    openModal();
+
+    setUserAddress('');
+    recaptcha.current.reset();
+
+    console.log(response);
   }
 
   const handleChangeUserAddress = (e) => {
     setUserAddress(e.target.value);
+  };
+
+  const handleChangeCaptcha = (token: string | null) => {
+    if (token) {
+      setCaptchaToken(token);
+    }
   };
 
   return (
@@ -28,14 +56,92 @@ export default function Home() {
         <form onSubmit={handleSendMatic}>
           <input 
             type="text"
-            placeholder="Type here" 
-            className="input input-bordered input-secondary w-full max-w-xl" 
+            placeholder="Wallet you want to send MATIC to" 
+            className="input input-bordered input-primary w-full max-w-xl mb-3" 
             value={userAddress}
             onChange={handleChangeUserAddress}
           />
-          <button className="btn bg-orange-500 hover:bg-orange-500 border border-orange-500 text-white text-bold">Send me MATIC!</button>
+
+          <div className="mb-3">
+            <ReCAPTCHA
+              size="normal"
+              sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITEKEY}
+              onChange={handleChangeCaptcha}
+              ref={recaptcha}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="btn w-full rounded-md px-4 py-2 text-white text-bold border border-transparent bg-orange-500 hover:bg-orange-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 focus-visible:ring-offset-2"
+            disabled={!captchaToken || !userAddress}
+          >
+            Send {process.env.NEXT_PUBLIC_MATIC_AMOUNT} MATIC to Mumbai network
+          </button>
         </form>
       </div>
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Your Matic is on the way!
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500">
+                      Your request has been successfully submitted.<br/>
+                      <a
+                        className="text-blue-500"
+                        target="_blank"
+                        rel="noreferrer"
+                        href={`https://mumbai.polygonscan.com/tx/${transactionHash}`}
+                      >
+                        You can check the transaction status clicking here.
+                      </a>
+                    </p>
+                  </div>
+
+                  <div className="mt-4">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md px-4 py-2 text-sm font-medium text-white border border-transparent bg-orange-500 hover:bg-orange-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 focus-visible:ring-offset-2"
+                      onClick={closeModal}
+                    >
+                      Got it, thanks!
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </>
   );
 }
