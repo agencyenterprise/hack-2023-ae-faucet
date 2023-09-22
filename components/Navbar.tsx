c1153c3120f706ec2b4826952661cf09686306ff
kevinkons <kevin.kons@hotmail.com>
2023-09-22 18:41:00 -0300

    Nav bar updates

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,4 @@
 import { Disclosure } from "@headlessui/react";
-import {
-  Bars3Icon,
-  XMarkIcon,
-} from "@heroicons/react/24/outline";import classNames from "classnames";
-
-import Link from "next/link";
 
 const Navbar = () => {
   return (
@@ -13,21 +7,9 @@ const Navbar = () => {
         <>
           <div className="w-full mx-auto">
             <div className="relative flex justify-between h-16">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button */}
-                <Disclosure.Button className="relative inline-flex items-center justify-center text-gray-400 rounded-md hover:bg-gray-100 hover:text-gray-500 focus:outline-none">
-                  <span className="absolute -inset-0.5" />
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block w-6 h-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block w-6 h-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
-                <div className="flex items-center flex-shrink-0">
-                  <Link href="/">
+              <div className=" first-letter:items-center flex-1 sm:items-stretch sm:justify-start">
+                <div className="flex items-center flex-shrink-0 justify-center ">
+                  <a href="https://ae.studio/" target="_blank" rel="noreferrer">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="58.8"
@@ -66,7 +48,7 @@ const Navbar = () => {
                         />
                       </g>
                     </svg>
-                  </Link>
+                  </a>
                 </div>
               </div>
             </div>
