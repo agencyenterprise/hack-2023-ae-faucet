cef7d89a5cfaf52f87c46e11ff7e52ba36d481c6
kevinkons <kevin.kons@hotmail.com>
2023-09-22 19:13:18 -0300

    changing banner

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -3,28 +3,11 @@ export default function HomeBanner() {
     <>
       <div className="flex flex-col items-center justify-center gap-4 mb-4 md:items-center md:flex-row">
         <div className="flex flex-col justify-center">
-          <h1 className="text-[#4E1E1A] text-left font-russo-one text-2xl md:text-[58px] md:leading-[63px] md:tracking-[-4.06px]">
-            {"Get tokens with the most"}
+          <h1 className="text-white text-center font-extrabold text-[48px] md:text-[58px] md:leading-[63px]">
+            GET FREE TOKENS FROM THE WORLD'S<br/>
+            MOST AWESOME AGENCY
+            <span className="font-semibold text-[16px]"> (That's us)</span>
           </h1>
-          <div className="flex flex-row justify-center">
-            <h1 className="inline-block text-center awesome-heading text-2xl md:text-[58px] md:leading-[63px] md:tracking-[-4.06px]">
-              awesome
-            </h1>
-            <h1 className="text-[#4E1E1A] ml-3 inline-block text-left font-russo-one text-2xl md:text-[58px] md:leading-[63px] md:tracking-[-4.06px]">
-              {"agency"}
-            </h1>
-          </div>
-          <h1 className="mt-4 flex justify-center code-collab-celebr">
-            {"We can build your project from the ground up!"}
-          </h1>
-          <a
-            href="https://ae.studio/"
-            target="noopener _blank"
-            rel="noopener noreferrer"
-            className="mt-2 text-orange-300 text-center"
-          >
-            Get to know us
-          </a>
         </div>
       </div>
     </>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const Navbar = () => {
   return (
-    <div className="w-full mx-auto mt-3">
+    <div className="w-full mx-auto mt-6 mb-20">
       <div className="relative flex justify-between h-16">
         <div className=" first-letter:items-center flex-1 sm:items-stretch sm:justify-start">
           <div className="flex items-center justify-center ">
@@ -10,8 +10,8 @@ const Navbar = () => {
               <Image
                 src="/aestudio-logo-light.png" // Provide the relative path to the image
                 alt="AE Studio Logo"
-                width={100} // Set the width of the image
-                height={100} // Set the height of the image
+                width={120} // Set the width of the image
+                height={120} // Set the height of the image
               />
             </a>
           </div>
