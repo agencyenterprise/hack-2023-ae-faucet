f21ecf1547b7079c267bb873ad4596b17bcdc08e
kevinkons <kevin.kons@hotmail.com>
2023-09-22 16:36:48 -0300

    changing modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import axios from 'axios';
 import ReCAPTCHA from 'react-google-recaptcha';
 import { Dialog, Transition } from '@headlessui/react'
 import { ethers } from "ethers";
+import LottieAnimation from "@/components/lottie";
+import AELogo from "@/components/AELogo";
 
 const HomeBanner = dynamic(() => import("@/components/HomeBanner"), {
   ssr: false,
@@ -69,10 +71,10 @@ export default function Home() {
       <div className="flex justify-center space-x-2">
         <form onSubmit={handleSendMatic}>
           <div className="mb-3">
-            <input 
+            <input
               type="text"
-              placeholder="Wallet you want to send MATIC to" 
-              className="input input-bordered input-primary w-full max-w-xl" 
+              placeholder="Wallet you want to send MATIC to"
+              className="input input-bordered input-primary w-full max-w-xl"
               value={userAddress}
               onChange={handleChangeUserAddress}
               required
@@ -126,48 +128,93 @@ export default function Home() {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
+                {
+                  // errorMessage ? (
+                  //   <div>Error</div>
+                  // ) : (
+                  // <SuccessModal closeModal={closeModal} transactionHash={transactionHash} />
+                  // )
+                }
                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
                     className="text-lg font-medium leading-6 text-gray-900"
                   >
-                    {errorMessage ? "Oooops, something went wrong :)" : "Your Matic is on the way!"}
+                    {errorMessage ? (
+                      <div className="text-center">
+                        {"Oooops, something went wrong :)"}
+                      </div>
+                    ) : (
+                      <div className="flex flex-col justify-center">
+                        <div className="h-16 flex justify-center">
+                          <LottieAnimation />
+                        </div>
+                        <p className="text-center">Your MATIC is on the way...</p>
+                      </div>
+                    )}
                   </Dialog.Title>
                   <div className="mt-2">
                     {errorMessage ? (
-                      <p className="text-sm text-red-500">
-                        {errorMessage}
-                      </p>
-                    ) : (
-                      <p className="text-sm text-gray-500">
-                        Your request has been successfully submitted.<br/>
-                        <a
-                          className="text-blue-500"
-                          target="_blank"
-                          rel="noreferrer"
-                          href={`https://mumbai.polygonscan.com/tx/${transactionHash}`}
+                      <div className="flex flex-col justify-center item">
+                        <p className="text-sm text-center text-red-500 mt-8 mb-8">
+                          {errorMessage}
+                        </p>
+                        <button
+                          type="button"
+                          className="w-32 inline-flex justify-center rounded-md px-4 py-2 text-sm font-medium text-white border border-transparent bg-orange-500 hover:bg-orange-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 focus-visible:ring-offset-2"
+                          onClick={closeModal}
                         >
-                          You can check the transaction status clicking here.
-                        </a>
-                      </p>
-                    )}
-                  </div>
+                          Got it, thanks!
+                        </button>
+                      </div>
+                    ) : (
+                      <div className="flex flex-col justify-center items-center">
+                        <div className="w-64 ">
+                          <div className="divider"></div>
+                        </div>
+                        <AELogo />
+                        <p className="text-sm text-black mt-5 mb-5 text-center">
+                          We hire the <span className="font-bold">best developers in the world</span> to
+                          work on the <span className="font-bold">coolest products</span>, on the absolute
+                          <span className="font-bold"> cutting edge of tech.</span>
 
-                  <div className="mt-4">
-                    <button
-                      type="button"
-                      className="inline-flex justify-center rounded-md px-4 py-2 text-sm font-medium text-white border border-transparent bg-orange-500 hover:bg-orange-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 focus-visible:ring-offset-2"
-                      onClick={closeModal}
-                    >
-                      Got it, thanks!
-                    </button>
+                          {/* Your request has been successfully submitted.<br /> */}
+                          {/* <a
+                            className="text-blue-500"
+                            target="_blank"
+                            rel="noreferrer"
+                            href={`https://mumbai.polygonscan.com/tx/${transactionHash}`}
+                            >
+                            You can check the transaction status clicking here.
+                          </a> */}
+                        </p>
+                        <div className="w-64">
+                          <div className="divider"></div>
+                        </div>
+                        <a
+                          href="https://ae.studio/join-us"
+                          target="_blank"
+                          className="mt-5 inline-flex justify-center rounded-md px-4 py-2 text-sm font-medium text-white border border-transparent bg-orange-500 hover:bg-orange-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 focus-visible:ring-offset-2"
+                          onClick={closeModal}
+                        >
+                          COME WORK FOR US!
+                        </a>
+                        <a
+                          href="https://ae.studio"
+                          className="text-orange-500 text-xs mt-7"
+                          target="_blank"
+                        >
+                          LEARN MORE
+                        </a>
+                      </div>
+                    )}
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
           </div>
         </Dialog>
-      </Transition>
+      </Transition >
     </>
   );
 }
