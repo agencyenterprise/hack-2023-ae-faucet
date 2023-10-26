6d227a9983d5018f483eddae68d5b7758c3e90e9
EgonStep <egonstep@hotmail.com>
2023-10-26 13:15:00 -0300

    added sentry

--- pages/pricing/index.tsx
+++ pages/pricing/index.tsx
@@ -7,7 +7,14 @@ import { PricingPage } from 'sds-projects';
 export default function Pricing() {
   return (
     <div className="-mt-5">
-      <PricingPage />
+      <PricingPage
+        freeTierFeatures={[
+          "Get FREE tokens",
+          "Unlimited access to the app",
+          "Quick and easy to use",
+          "No ads",
+        ]}
+      />
     </div>
   );
 }
